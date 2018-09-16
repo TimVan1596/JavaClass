67e90c82bfefa7e60aa7e95c9594df6db1a8614c
fy <928247992@qq.com>
2018-09-16 13:06:45 +0800

    把输入的学生对象信息存入文本文件再读出来。

--- src/com/smallfangyu/intout/TestCollections.java
+++ src/com/smallfangyu/intout/TestCollections.java
@@ -7,14 +7,48 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.ArrayList;
+import java.util.*;
 
 import com.smallfangyu.intout.Student;
 
 
 public class TestCollections {
+	static int no=0;
+	static int age=0;
+	static Scanner  scanner = new Scanner(System.in);
 
-	
+	//学号输入
+	public static void inputno(){
+		//Scanner scanner = new Scanner(System.in);
+         try {
+			Scanner  scanner = new Scanner(System.in);
+			System.out.println("输入学生学号：");
+			no = scanner.nextInt();
+			scanner.nextLine();
+		}catch(InputMismatchException e){
+			System.out.println("辣鸡,输错了，重输。" );
+			inputno();
+		}
+
+	}
+
+   //年龄输入
+	public static void inputage(){
+		//Scanner scanner = new Scanner(System.in);
+
+		try {
+			Scanner  scanner = new Scanner(System.in);
+			System.out.println("输入学生年龄：");
+			age = scanner.nextInt();
+			scanner.nextLine();
+		}catch(InputMismatchException e){
+			System.out.println("辣鸡,输错了，重输。" );
+			inputage();
+		}
+
+	}
+
+
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
@@ -36,10 +70,31 @@ public class TestCollections {
 //		}
 		  // 留个思考： 集合里装入3个学生， 你能按照姓名排序（中文、英文） 输出名？ 按照年龄排序输出吗？
 		//构建2个学生
-		
-			Student student1=new Student(1, "李雷",22);
-			Student student2=new Student(2, "韩梅梅",21);
-			Student student3=new Student(3, "吉姆",23);
+		boolean jud=false;
+
+		ArrayList<Student> students = new ArrayList<Student>();
+    do {
+
+    	inputno();
+		System.out.println("输入学生姓名：");
+		String name = scanner.next();
+		inputage();
+
+		//添加student数据
+		students.add(new Student(no, name, age));
+
+		System.out.println("判断是否继续y/n");
+
+		String judge = scanner.next();
+		if(judge.equals("y")){
+			jud=true;
+		}else if(judge.equals("n")){
+			break;
+		}
+	}while(jud);
+
+			//Student student2=new Student(2, "韩梅梅",21);
+			//Student student3=new Student(3, "吉姆",23);
 			
 			//创建文件路径
 			File file=new File("F:\\新建文件夹\\test.txt");
@@ -51,10 +106,10 @@ public class TestCollections {
 			FileOutputStream fos=new FileOutputStream(file);
 			
 			try {
-				ArrayList<Student> students=new ArrayList<Student>();
-				students.add(student1);
-				students.add(student2);
-				students.add(student3);
+				//ArrayList<Student> students=new ArrayList<Student>();
+				//students.add(student1);
+				//students.add(student2);
+				//students.add(student3);
 			ObjectOutputStream out=new ObjectOutputStream(fos);
 			
 			out.writeObject(students);
@@ -71,9 +126,9 @@ public class TestCollections {
 			try {
 				
 				ObjectInputStream in=new ObjectInputStream(fis);
-				ArrayList<Student> students=(ArrayList<Student>) in.readObject();
+				ArrayList<Student> stus=(ArrayList<Student>) in.readObject();
 				in.close();
-				for(Student stu:students) {
+				for(Student stu:stus) {
 				 System.out.println(stu);}
 			} catch (Exception e) {
 				// TODO 自动生成的 catch 块
