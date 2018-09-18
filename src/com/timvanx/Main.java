fc8e9e797421fae3892342c6b1fd30d5a76d6244
TimVan <877020296@qq.com>
2018-09-18 22:33:55 +0800

    提交作业

--- src/com/timvanx/Main.java
+++ src/com/timvanx/Main.java
@@ -1,27 +1,40 @@
 package com.timvanx;
 
-public class Main {
-    public static void main(String[] args) {
-        Child child = new Child();
+//1、简答题：简述this关键字,super关键字的用法
+// this:
+// (1)this调用本类中的属性，也就是类中的成员变量；
+// (2)this调用本类中的其他方法；
+// (3)this调用本类中的其他构造方法，调用时要放在构造方法的首行。
+//super:super能够用来訪问超类的构造方法和被子类所隐藏的方法。
+
+//2、定义一个类CalF，完成加、减方法，然
+//后再定义一个派生类CalS，完成乘、除方法
+
+class CalF{
+    public double add(double a,double b) {
+        double ret = a+b;
+        return ret;
+    }
+
+    public double minis(double a,double b) {
+        double ret = a-b;
+        return ret;
     }
 }
 
-class Parent {
-    static {
-        System.out.println("parent static code block");
+class CalS extends CalF{
+    public double mutiple(double a,double b) {
+        return a*b;
     }
 
-    public Parent() {
-        System.out.println("parent constructor");
+    public String chu(double a,double b) {
+
+        if (b==0) {
+            return "分母为0";
+        }
+        Double ret = a/b;
+
+        return ret.toString();
     }
 }
 
-class Child extends Parent {
-    static {
-        System.out.println("child static code block");
-    }
-
-    public Child() {
-        System.out.println("child constructor");
-    }
-}
--- src/com/timvanx/homework/Suv.java
+++
@@ -1,6 +0,0 @@
-package com.timvanx.homework;
-
-/**
- * @author TimVan
- * @date 2018/9/12 20:58
- */
