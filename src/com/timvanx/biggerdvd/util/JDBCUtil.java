f9744a45e319dc24d913c9e9321c0838f54b5d47
TimVan <877020296@qq.com>
2018-09-20 22:32:45 +0800

    更新数据库util

--- src/com/timvanx/biggerdvd/util/JDBCUtil.java
+++ src/com/timvanx/biggerdvd/util/JDBCUtil.java
@@ -1,31 +1,29 @@
 package com.timvanx.biggerdvd.util;
 
-import javax.sound.midi.SoundbankResource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * 封装MySQL数据库的工具类
  * <p>包含常用的update、select、delete操作</p>
  * <ul>本JDBC操作数据库的步骤
- *  <li>注册驱动 (只做一次)</li>
- *  <li>建立连接(Connection)</li>
- *  <li>创建执行SQL的语句(Statement)</li>
- *  <li>执行语句并处理执行结果(ResultSet)</li>
- *  <li>释放资源</li>
+ * <li>注册驱动 (只做一次)</li>
+ * <li>建立连接(Connection)</li>
+ * <li>创建执行SQL的语句(Statement)</li>
+ * <li>执行语句并处理执行结果(ResultSet)</li>
+ * <li>释放资源</li>
  * </ul>
+ *
  * @author TimVan
  * @date 2018-9-17 17:12:03
  * @since 4.0
  */
-public class MysqlDAO {
+public class JDBCUtil {
     /**
-     设置驱动、url、用户名、密码
-     通过db.properties配置
+     * 设置驱动、url、用户名、密码
+     * 通过db.properties配置
      */
     private static String DB_URL;
     private static String USER;
@@ -39,14 +37,14 @@ public class MysqlDAO {
     /**
      * 初始化JDBC-MySQL连接(只做一次)
      * <ul>
-     *   <li>初始化数据库连接配置（.properties）</li>
-     *   <li>注册连接</li>
+     * <li>初始化数据库连接配置（.properties）</li>
+     * <li>注册连接</li>
      * </ul>
      */
     private static void init() {
         //初始化数据库连接配置
         Properties properties = new Properties();
-        InputStream in = MysqlDAO.class.getClassLoader()
+        InputStream in = JDBCUtil.class.getClassLoader()
                 .getResourceAsStream("db.properties");
         /**
          * 设置驱动、url、用户名、密码
@@ -63,7 +61,7 @@ public class MysqlDAO {
 
         /**
          设置驱动、url、用户名、密码
-         通过db.properties配置
+         通过db.properties文件配置
          */
         String JDBC_DRIVER = properties.getProperty("JDBC_DRIVER");
         DB_URL = properties.getProperty("DB_URL");
@@ -103,13 +101,18 @@ public class MysqlDAO {
 
     /**
      * select - MySQL方法
-     * @param tableName 表名
+     *
+     * @param tableName  表名
      * @param tableField 字段
      * @param tableWhere 条件
      */
-    public static void select(String tableName, ArrayList<String> tableField, String tableWhere) {
+    public static List<Map<Object, Object>> select
+    (String tableName, ArrayList<String> tableField
+            , String tableWhere) {
         Connection conn = null;
         Statement stmt = null;
+
+        List<Map<Object, Object>> list = new ArrayList<>();
 
         try {
             //连接数据库
@@ -121,28 +124,34 @@ public class MysqlDAO {
             stringBuilder.append(arrayListToString(tableField));
             stringBuilder.append(" from ");
             stringBuilder.append(tableName);
-            stringBuilder.append(" where ");
-            stringBuilder.append(tableWhere);
+            if (tableWhere != null) {
+                stringBuilder.append(" where ");
+                stringBuilder.append(tableWhere);
+            }
+
             String sql = stringBuilder.toString();
 
             System.out.println("---" + sql);
 
             ResultSet rs = stmt.executeQuery(sql);
-
-            // 展开结果集数据库
+            // 获得结果集结构信息（元数据）
+            ResultSetMetaData md = rs.getMetaData();
+            // 获取select结果的元组行数
+            int columnCount = md.getColumnCount();
+            // ResultSet转List<Map>数据结构
+            // next用于移动到ResultSet的下一行，使下一行成为当前行
             while (rs.next()) {
-                // 通过字段检索
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String password = rs.getString
-                        ("password");
-
-                //输出数据
-                System.out.print("id: " + id);
-                System.out.print(", 用户名: " + name);
-                System.out.print(", 密码: " + password);
-                System.out.print("\n");
+                Map<Object, Object> map =
+                        new HashMap<>(columnCount);
+                for (int i = 1; i <= columnCount; i++) {
+                    // 遍历获取对当前行的每一列的键值对，put到map中
+                    // rs.getObject(i) 获得当前行某一列字段的值
+                    map.put(md.getColumnName(i).toLowerCase()
+                            , rs.getObject(i));
+                }
+                list.add(map);
             }
+
             // 完成后关闭
             rs.close();
             stmt.close();
@@ -157,6 +166,8 @@ public class MysqlDAO {
             //释放资源
             close(conn, stmt);
         }
+
+        return list;
     }
 
     /**
@@ -184,14 +195,25 @@ public class MysqlDAO {
         return stringBuffer.toString();
     }
 
+
     public static void main(String[] args) {
         String tableName = "account";
-        ArrayList<String> tableField = new ArrayList<>();
-        tableField.add("id");
-        tableField.add("name");
-        tableField.add("password");
-        String tableWhere = "name = " + "'account'";
-        select(tableName, tableField, tableWhere);
+        ArrayList<String> tableField = new ArrayList<String>() {{
+            add("id");
+            add("name");
+            add("password");
+        }};
+
+        List<Map<Object, Object>> list =
+                select(tableName, tableField, null);
+        for (Map<Object, Object> cache :list) {
+            for (Map.Entry<Object, Object> entry
+                    : cache.entrySet()) {
+                System.out.println(entry.getKey() + ":" + entry.getValue());
+            }
+        }
+
+
     }
 
 
