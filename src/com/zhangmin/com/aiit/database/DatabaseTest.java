25e2281a3448262cd8f71c3e3e8957e3792b6d69
zhangmin <2668233491@qq.com>
2018-09-28 09:23:00 +0800

    PrepareStatement用法

--- src/com/zhangmin/com/aiit/database/DatabaseTest.java
+++ src/com/zhangmin/com/aiit/database/DatabaseTest.java
@@ -1,40 +1,57 @@
 package com.zhangmin.com.aiit.database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 
 public class DatabaseTest {
     public static void main(String[] args){
         //1.加载驱动
-
-
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-
-
+            Class.forName("com.mysql.cj.jdbc.Driver");
             //2.创建连接
             Connection conn;
             conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/stu"
                             + "?useSSL=false&serverTimezone=UTC&characterEncoding=UTF8"
                     ,"root","123456");
             //3.准备sql
-            String sql = "INSERT INTO tb_student(stuName,stuAddress,stuScore) value ('HH','安徽信息工程学院',70)";
+            //String sql1 = "INSERT INTO tb_student(stuName,stuAddress,stuScore) value ('HH','安徽信息工程学院',70)";
+            String sql2 = "SELECT stuID,stuName,stuAddress,stuScore FROM tb_student";
+            //String sql3 ="UPDATE tb_student set stuScore = stuScore+1 where stuName='张三'";
+            //String sql4 = "DELETE FROM tb_student where stuName = 'HH'";
             //4.执行处理
-            PreparedStatement pre = conn.prepareStatement(sql);
+            PreparedStatement pre;
+            // pre= conn.prepareStatement(sql1);//增
+              pre = conn.prepareStatement(sql2);//查
+             //pre = conn.prepareStatement(sql3);//改
+               //pre = conn.prepareStatement(sql4);//删
+            /*
+            * 查询操作
+            */
+             ResultSet rs = pre.executeQuery();
+            System.out.println("序号"+"\t"+"姓名"+"\t"+"地址"+"\t"+"分数");
+             while (rs.next()){
+                 int id = rs.getInt("stuID");
+                 String name = rs.getString("stuName");
+                 String address = rs.getString("stuAddress");
+                 float score = rs.getFloat("stuScore");
+                 System.out.println(id+"\t"+name+"\t"+address+"\t"+score);
+                 //System.out.println("查询到结果"+);
+             }
             //5.返回结果
-            int ret = pre.executeUpdate();
-            if(ret>=0){
-                System.out.println("执行成功");
-            }
-
+            /*
+            * 插入、修改、删除操作
+            */
+//            int ret = pre.executeUpdate();
+//            if(ret>=0){
+//                System.out.println("执行成功");
+//            }
+            //6.关闭连接
+             rs.close();
+             pre.close();
+             conn.close();
         } catch (ClassNotFoundException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
---
+++ src/com/zhangmin/com/aiit/datafengzhuang/DatabaseModel.java
@@ -0,0 +1,51 @@
+package com.zhangmin.com.aiit.datafengzhuang;
+
+import java.sql.*;
+
+public class DatabaseModel {
+    //1.加载驱动
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    //2.建立连接
+    public static Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/stu?useSSL=false&serverTimezone=UTC&characterEncoding=UTF8";
+        String user = "root";
+        String password = "123456";
+        Connection conn = null;
+        {
+            try {
+                conn = DriverManager.getConnection(url, user, password);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+             return  conn;
+    }
+    //3.测试
+    //将指定学生分数加+1
+    public static void addScore(int stuID){
+        Connection conn = getConnection();
+        String sql = "update tb_student set stuScore = stuScore+1 where stuID=?";
+        PreparedStatement pre;
+        try {
+            pre = conn.prepareStatement(sql);
+            pre.setInt(1,stuID);
+
+            int iRet = pre.executeUpdate();
+            if(iRet >= 0){
+                System.out.println("执行成功");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+          addScore(3);
+    }
+
+}
