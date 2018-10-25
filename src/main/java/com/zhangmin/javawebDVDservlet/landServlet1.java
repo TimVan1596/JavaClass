a040c04913933053db096cf140ead27b7cbd61dc
zhangmin <2668233491@qq.com>
2018-10-25 17:49:18 +0800

    修改登录注册

--- src/main/java/com/zhangmin/javawebDVDservlet/landServlet1.java
+++ src/main/java/com/zhangmin/javawebDVDservlet/landServlet1.java
@@ -1,4 +1,4 @@
-package com.antianbao.javaWebDvd;
+package com.zhangmin.javawebDVDservlet;
 
 import com.antianbao.javaWebDvd.user.JDBCUtilUser;
 
@@ -9,31 +9,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "landServlet1",urlPatterns = {"/login.do"})
-public class landServlet extends HttpServlet {
+@WebServlet(name = "landServlet1",urlPatterns = {"/zm/landServlet1.do"})
+public class landServlet1 extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //post解决中文乱码
         request.setCharacterEncoding("utf-8");
         //获取输入信息
         String userName = request.getParameter("username");
         String userPassword = request.getParameter("userpassword");
-        if(userName.equals("用户账号") || userPassword.equals("账号密码") || userName.equals("") || userPassword.equals("")){
+        if(userName.equals("") || userPassword.equals("")){
             //跳转到新的界面
             request.setAttribute("MSG", "账号密码不能为空！");
-            request.setAttribute("DL", "意思下");
-            request.getRequestDispatcher("./atb/javaWebDvdLogin.jsp").forward(request, response);
+            request.getRequestDispatcher("https://daohang.qq.com/?unc=Af31062").forward(request, response);
         }else{
             JDBCUtilUser jdbcUtil = new JDBCUtilUser();
             int jd = jdbcUtil.isReally(userName, userPassword);
             if(jd > 0){
                 //跳转到新的界面
                 request.setAttribute("MSG", "登陆成功！");
-                request.getRequestDispatcher("./atb/javaWebDvd/display.jsp").forward(request, response);
+                //./atb/javaWebDvd/display.jsp
+                request.getRequestDispatcher("https://daohang.qq.com/?unc=Af31062").forward(request, response);
             }else{
                 //输出登陆失败
                 request.setAttribute("MSG", "账号密码错误或用户不存在！");
-                request.setAttribute("DL", "意思下");
-                request.getRequestDispatcher("./atb/javaWebDvdLogin.jsp").forward(request, response);
+                request.getRequestDispatcher("https://daohang.qq.com").forward(request, response);
             }
         }
     }
@@ -41,6 +40,6 @@ public class landServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //get解决中文乱码
         //name = new String(name.getBytes("ISO-8859-1"),"utf-8");
-        request.getRequestDispatcher("./atb/javaWebDvd/display.jsp").forward(request, response);
+        request.getRequestDispatcher("https://daohang.qq.com").forward(request, response);
     }
 }
---
+++ web/zm/DVD/jsp/getPassword.jsp
@@ -0,0 +1,34 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/10/24/024
+  Time: 20:16
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>找回密码</title>
+</head>
+<body>
+<div style="text-align: center">
+    <h4>找回密码</h4>
+    <form action="abc.do" method="post" onsubmit="return returnPassword()">
+        请输入账号：<input type=" text" name="username" id="returnName"/><br>
+        <input type="submit" value="提交"/>
+    </form>
+</div>
+<script>
+    function returnPassword(){
+        var  name=document.getElementById("returnName").value;
+        if(name!=""){
+            return true;
+        }else{
+            alert("亲，账号不能为空哦~");
+            return false;
+        }
+
+    }
+</script>
+</body>
+</html>
