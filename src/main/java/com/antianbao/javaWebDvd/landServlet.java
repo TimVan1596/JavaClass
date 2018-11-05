f4a6fd537e67f7482216f414584c43004d968b4e
安天宝 <32336077@qq.com>
2018-11-05 17:15:50 +0800

    bootstrap CSS和JS

--- src/main/java/com/antianbao/javaWebDvd/landServlet.java
+++ src/main/java/com/antianbao/javaWebDvd/landServlet.java
@@ -28,7 +28,6 @@ public class landServlet extends HttpServlet {
             if(jd > 0){
                 //跳转到新的界面
                 request.setAttribute("MSG", "登陆成功！");
-                request.setAttribute("user", userName);
                 request.getRequestDispatcher("./atb/javaWebDvd/display.jsp").forward(request, response);
             }else{
                 //输出登陆失败
