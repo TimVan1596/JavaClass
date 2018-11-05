67d831c316090b9e0d64067e32173fe2443885eb
zhangmin <2668233491@qq.com>
2018-11-05 14:46:05 +0800

    删除图书管理系统

--- src/main/java/com/zhangmin/javawebDVDservlet/getPassword.java
+++
@@ -1,7 +0,0 @@
-package com.zhangmin.javawebDVDservlet;
-
-import javax.servlet.annotation.WebServlet;
-
-@WebServlet(name="getPassword",urlPatterns ={"/zm/DVD/getPassword.do"} )
-public class getPassword {
-}
--- src/main/java/com/zhangmin/javawebDVDservlet/registerServlet.java
+++
@@ -1,9 +0,0 @@
-package com.zhangmin.javawebDVDservlet;
-
-
-import javax.servlet.annotation.WebServlet;
-
-@WebServlet(name = "registerServlet",
-        urlPatterns = {"/zm/DVD/register.do"}, loadOnStartup = 1)
-public class registerServlet {
-}
