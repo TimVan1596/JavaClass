1424d10f058117ff3d316e8d333ba2db320c7e53
TimVan <877020296@qq.com>
2018-11-14 01:36:45 +0800

    更新图论

--- src/main/java/com/timvanx/algorithm/graphy/Date1108.java
+++ src/main/java/com/timvanx/algorithm/graphy/Date1108.java
@@ -3,6 +3,40 @@ package com.timvanx.algorithm.graphy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 无向图 类
+ * @author TimVan
+ * @date 2018年11月14日00:14:04
+ * */
+class UnDirectGraph {
+
+    /**
+     * elem = 权
+     * vector = 顶点个数
+     * edge = 边的个数
+     * */
+    private List<List<Integer>> elem;
+    private int vector;
+    private int edge;
+
+    public UnDirectGraph(int vector, int edge) {
+        this.vector = vector;
+        this.edge = edge;
+    }
+
+    public void createVector(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入顶点的个数  ");
+
+
+        System.out.println("请输入"+this.edge+"条边");
+    }
+}
+
+
 /**
  *  建立有向图
  * @author TimVan
@@ -13,7 +47,10 @@ public class Date1108 {
 
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(Date1108.class);
+        int vector = 5;
+        int edge = 5;
+        UnDirectGraph unDirectGraph = new UnDirectGraph(vector,edge);
 
-        logger.debug("这他妈写的什么吊程序");
+
     }
 }
