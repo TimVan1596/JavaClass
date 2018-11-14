40859be7d1efffbbdc68c41f35c8afdbaeeddc12
安天宝 <32336077@qq.com>
2018-11-14 15:38:02 +0800

    layui文档部分 学完

--- src/main/java/com/antianbao/javaWebDvd/jsp/choice/imageServlet.java
+++ src/main/java/com/antianbao/javaWebDvd/jsp/choice/imageServlet.java
@@ -1,30 +1,132 @@
-package com.antianbao.javaWebDvd.jsp.choice;
-
-import com.antianbao.javaWebDvd.dvd.Dvd;
-import com.antianbao.javaWebDvd.dvd.JDBCUtilDvd;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.*;
-
-@WebServlet(name = "imageServlet",urlPatterns = {"/imageServlet.do"})
-public class imageServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //post解决中文乱码
-        request.setCharacterEncoding("utf-8");
-        System.out.println(123);
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //get解决中文乱码
-        //name = new String(name.getBytes("ISO-8859-1"),"utf-8");
-
-    }
-}
+//package com.antianbao.javaWebDvd.jsp.choice;
+//
+//import com.alibaba.fastjson.JSON;
+//import com.timvanx.biggerdvd.util.QiniuCloudUtil;
+//import org.apache.tomcat.util.http.fileupload.FileItem;
+//import org.apache.tomcat.util.http.fileupload.RequestContext;
+//import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+//import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+//import org.apache.tomcat.util.http.fileupload.servlet.ServletRequestContext;
+//
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.*;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+///**
+// * 编辑的DVD的信息
+// * @author TimVan
+// */
+//@WebServlet(name = "imageServlet",urlPatterns = {"/imageServlet.do"})
+//public class imageServlet extends HttpServlet {
+//
+//    /**
+//     * UPLOAD_DIRECTORY = 上传文件存储目录
+//     * */
+//    private static final String UPLOAD_DIRECTORY = "upload";
+//
+//    private static final int MEMORY_THRESHOLD
+//            = 1024 * 1024 * 3;
+//    private static final int MAX_FILE_SIZE      = 1024 * 1024 * 40;
+//    private static final int MAX_REQUEST_SIZE   = 1024 * 1024 * 50;
+//
+//    @Override
+//    public void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws IOException {
+//        doPost(request, response);
+//    }
+//
+//    @Override
+//    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        request.setCharacterEncoding("UTF-8");
+//        response.setContentType("application/text; charset=utf-8");
+//        PrintWriter out = response.getWriter();
+//        //返回值jSON,键值对
+//        Map<String, Object> ret = new HashMap<>(1);
+//
+//        // 检测是否为多媒体上传
+//        if (!org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload.isMultipartContent(request)) {
+//            // 如果不是则停止
+//            ret.put("error", 1);
+//            ret.put("errorInfo", " 表单必须包含 enctype=multipart/form-data");
+//        }else{
+//            // 配置上传参数
+//            org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory factory = new DiskFileItemFactory();
+//            // 设置内存临界值 - 超过后将产生临时文件并存储于临时目录中
+//            factory.setSizeThreshold(MEMORY_THRESHOLD);
+//            // 设置临时存储目录
+//            factory.setRepository(
+//                    new File(System
+//                            .getProperty("java.io.tmpdir")));
+//
+//            org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload upload = new ServletFileUpload(factory);
+//
+//            // 设置最大文件上传值
+//            upload.setFileSizeMax(MAX_FILE_SIZE);
+//            // 设置最大请求值 (包含文件和表单数据)
+//            upload.setSizeMax(MAX_REQUEST_SIZE);
+//            // 中文处理
+//            upload.setHeaderEncoding("UTF-8");
+//
+//            // 构造临时路径来存储上传的文件
+//            // 这个路径相对当前应用的目录
+//            String uploadPath = request.getServletContext()
+//                    .getRealPath("./") + File.separator
+//                    + UPLOAD_DIRECTORY;
+//
+//            // 如果目录不存在则创建
+//            File uploadDir = new File(uploadPath);
+//            if (!uploadDir.exists()) {
+//                uploadDir.mkdir();
+//            }
+//
+//            try {
+//
+//                RequestContext requestContext
+//                        = new ServletRequestContext(request);
+//                // 解析请求的内容提取文件数据
+//                @SuppressWarnings("unchecked")
+//                List<org.apache.tomcat.util.http.fileupload.FileItem> formItems = upload.parseRequest(requestContext);
+//
+//                if (formItems != null && formItems.size() > 0) {
+//                    // 迭代表单数据
+//                    for (FileItem item : formItems) {
+//                        // 处理不在表单中的字段
+//                        if (!item.isFormField()) {
+//
+//
+//                            String fileName = System.currentTimeMillis()+
+//                                    "."+(item.getName().split("\\."))[1];
+//                            String filePath = uploadPath + File.separator
+//                                    + fileName;
+//                            File storeFile = new File(filePath);
+//                            // 在控制台输出文件的上传路径
+//                            System.out.println(filePath);
+//                            // 保存文件到硬盘
+//                            item.write(storeFile);
+//
+//                            String retURL = QiniuCloudUtil.uploadStreamToCloud(
+//                                    new FileInputStream(storeFile)
+//                            );
+//
+//                            ret.put("error", 0);
+//                            ret.put("data", retURL);
+//
+//                        }
+//                    }
+//                }
+//            } catch (Exception ex) {
+//                ret.put("error", 2);
+//                ret.put("errorInfo", ex.getMessage());
+//            }
+//        }
+//
+//        //使用 Alibaba fastJson 序列化 ret
+//        String retJson = JSON.toJSONString(ret);
+//        out.write(retJson);
+//    }
+//
+//}
--- web/atb/test/atbText.jsp
+++ web/atb/test/atbText.jsp
@@ -1,61 +1,102 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: Administrator
-  Date: 2018\11\13 0013
-  Time: 8:09
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>奖牌排序</title>
-    <%--layui引用--%>
-    <script src="../../common/util/layui/layui.js"></script>
-    <link rel="stylesheet" href="../../common/util/layui/css/layui.css">
-</head>
-<body>
-<div id="view-member-div" style="padding: 1% 5%;display: none">
-    <table class="layui-table"  id="view-member-table"></table>
-</div>
+<%--&lt;%&ndash;--%>
+  <%--添加新DVD页面 （iFrame）--%>
+  <%--User: TimVan--%>
+  <%--Date: 2018/10/18--%>
+  <%--Time: 10:26--%>
+<%--&ndash;%&gt;--%>
+<%--<%@ page contentType="text/html;charset=UTF-8" language="java" %>--%>
+<%--<html>--%>
+<%--<head>--%>
+    <%--<style>--%>
+        <%--.layui-upload-img{--%>
+            <%--width: 92px; height: 92px;--%>
+            <%--margin: 0 10px 10px 0;}--%>
 
-<!--弹出-->
-<script>
-    $(".layui-table").click(function () {
-        layer.open({
-            type: 1,
-            content: $('#view-member-div'), //这里content是一个DOM，注意：最好该元素要存放在body最外层，否则可能被其它的相对元素所影响
-            title : '查看会员',
-            area: ['800px', '420px'],
-            success: function(layero, index){
-                layui.use('table', function(){
-                    var table = layui.table;
-                    var cols =  [[ //标题栏
-                        {field:'num', title: '序号',align: 'center'}
-                        ,{field:'cardNum', title: '会员卡号',align: 'center'}
-                        ,{field:'Name', title: '会员姓名',align: 'center'}
-                        ,{field:'sex', title: '性别',align: 'center'}
-                        ,{field:'grade', title: '会员等级',align: 'center'}
-                        ,{field:'tel', title: '手机号码',align: 'center'}
-                        ,{field:'balance',title: '剩余金额',align: 'center'}
-                        ,{field:'remainingPoints', title: '剩余积分',align: 'center'}
-                        ,{field:'time', title: '领卡时间',align: 'center'}
-                    ]]
-                    //展示已知数据
-                    table.render({
-                        elem: '#view-member-table'
-                        , url: './user'
-                        ,size:'sm'
-                        ,cols:cols
-                        ,even: true
-                        , height: '300'
-                        ,page: true //是否显示分页
-                    });
+    <%--</style>--%>
+    <%--<script src="../../common/util/layui/layui.js"></script>--%>
+    <%--<link rel="stylesheet" href="../../common/util/layui/css/layui.css">--%>
+<%--</head>--%>
+<%--<body>--%>
 
-                });
-            }
+<%--<form style="margin: 5%" class="layui-form layui-form-pane">--%>
+    <%--<br>--%>
+    <%--&lt;%&ndash;DVD名称&ndash;%&gt;--%>
+    <%--<div class="layui-form-item">--%>
+        <%--<label class="layui-form-label">DVD名称--%>
+        <%--</label>--%>
+        <%--<div class="layui-input-block">--%>
+            <%--<input type="text" name="name"--%>
+                   <%--autocomplete="off" lay-verify="name"--%>
+                   <%--placeholder="请不要输入特殊字符"--%>
+                   <%--class="layui-input">--%>
+        <%--</div>--%>
+    <%--</div>--%>
+    <%--&lt;%&ndash;上传封面&ndash;%&gt;--%>
+    <%--<div class="layui-upload">--%>
 
-        });
-    })
-</script>
-</body>
-</html>
+        <%--<button type="button"--%>
+                <%--class="layui-btn  layui-btn-normal"--%>
+                <%--id="preview-upload">--%>
+            <%--<i class="layui-icon">&#xe67c;</i>--%>
+            <%--上传封面</button>--%>
+        <%--<div class="layui-upload-list">--%>
+            <%--<img class="layui-upload-img" id="preview-upload-img">--%>
+            <%--<p id="demoText"></p>--%>
+        <%--</div>--%>
+
+        <%--<input type="hidden" name="preview" id="preview-hidden">--%>
+    <%--</div>--%>
+    <%--&lt;%&ndash;提交按钮&ndash;%&gt;--%>
+    <%--<div style="margin-top: 1%">--%>
+        <%--<button class="layui-btn  layui-btn-normal--%>
+                    <%--layui-btn-radius layui-btn-fluid"--%>
+                <%--lay-filter = "addSubmmit"--%>
+                <%--lay-submit="" >添加</button>--%>
+    <%--</div>--%>
+<%--</form>--%>
+
+<%--<script>--%>
+    <%--layui.use(['upload','form'], function(){--%>
+        <%--var upload = layui.upload;--%>
+
+        <%--//DVD预览封面上传--%>
+        <%--var uploadInst = upload.render({--%>
+            <%--elem: '#preview-upload'--%>
+            <%--,url:--%>
+                <%--'../../imageServlet.do'--%>
+            <%--,before: function(obj){--%>
+
+                <%--let uploadLoading = layer.msg('上传中', {--%>
+                    <%--icon: 16--%>
+                    <%--,offset:['15%','15%']--%>
+                    <%--,shade: 0.01--%>
+                <%--});--%>
+                <%--//预读本地文件示例，不支持ie8--%>
+                <%--obj.preview(function(index, file, result){--%>
+                    <%--$('#preview-upload-img').attr('src', result);                       //图片链接（base64）--%>
+                <%--});--%>
+
+            <%--}--%>
+            <%--,done: function(ret){--%>
+
+                <%--let data = ret['data'];--%>
+                <%--$('#preview-hidden').val(data);--%>
+                <%--$('#preview-upload-img').attr('src', data);--%>
+
+                <%--//关闭上传loading--%>
+                <%--layer.closeAll();--%>
+
+            <%--}--%>
+            <%--,error: function(){--%>
+                <%--//演示失败状态，并实现重传--%>
+                <%--var demoText = $('#demoText');--%>
+                <%--demoText.html('<span style="color: #FF5722;">上传失败</span> <a class="layui-btn layui-btn-xs demo-reload">重试</a>');--%>
+                <%--demoText.find('.demo-reload').on('click', function(){--%>
+                    <%--uploadInst.upload();--%>
+                <%--});--%>
+            <%--}--%>
+        <%--});--%>
+    <%--});--%>
+<%--</script>--%>
+<%--</body>--%>
+<%--</html>--%>
