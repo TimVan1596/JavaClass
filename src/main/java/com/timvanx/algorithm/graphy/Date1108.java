70fe2cfb7f5cefb37134dec6ad3a2644353190db
TimVan <877020296@qq.com>
2018-11-18 22:25:01 +0800

    更新无向图

--- src/main/java/com/timvanx/algorithm/graphy/Date1108.java
+++ src/main/java/com/timvanx/algorithm/graphy/Date1108.java
@@ -3,7 +3,6 @@ package com.timvanx.algorithm.graphy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -18,33 +17,47 @@ class UnDirectGraph {
      * vector = 顶点个数
      * edge = 边的个数
      * */
-    private List<List<Integer>> elem;
+    private int[][] elem;
     private int vector;
     private int edge;
 
     public UnDirectGraph(int vector, int edge) {
         this.vector = vector;
         this.edge = edge;
+        elem = new int[vector][vector];
+
+        drawEdge();
     }
 
-    public void createVector(){
+    //绘制无向图的边
+    private void drawEdge(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入顶点的个数");
-        vector = scanner.nextInt();
-        System.out.println("请输入边的个数");
-        edge = scanner.nextInt();
 
 
-        System.out.println("请输入"+this.edge+"条边");
+        System.out.println("请输入"+this.edge+"条边的起点和终点");
         for (int i = 0; i <edge ; i++) {
             int edgeVecStart = 0,edgeVecEnd = 0;
             System.out.println("请输入第"+i+"条边的起点");
             edgeVecStart = scanner.nextInt();
             System.out.println("第"+i+"条边的终点");
             edgeVecEnd = scanner.nextInt();
+            elem[edgeVecStart][edgeVecEnd] = 1;
+            elem[edgeVecEnd][edgeVecStart] = 1;
 
 
         }
+    }
+
+    //打印无向图（邻接矩阵）
+    public void printUnDirectGraph(){
+        for (int i = 0; i < vector; i++) {
+            for (int j = 0; j < vector; j++) {
+                System.out.print(elem[i][j]+" \t");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
     }
 }
 
@@ -62,6 +75,7 @@ public class Date1108 {
         int vector = 5;
         int edge = 5;
         UnDirectGraph unDirectGraph = new UnDirectGraph(vector,edge);
+        unDirectGraph.printUnDirectGraph();
 
 
     }
