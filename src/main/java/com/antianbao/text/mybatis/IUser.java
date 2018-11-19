34e8cd068f3b8683fb9b1f413bbac8b3af8849d7
安天宝 <32336077@qq.com>
2018-11-19 11:14:41 +0800

    mybatis

---
+++ src/main/java/com/antianbao/text/mybatis/IUser.java
@@ -0,0 +1,23 @@
+package com.antianbao.text.mybatis;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+/**
+ *
+ * @author yiibai
+ *
+ */
+public interface IUser {
+    //@Select("select * from user where id= #{id}")
+    //public User getUserByID(int id);
+    public List<User> getUserList();
+
+    public void insertUser(User user);
+
+    public void updateUser(User user);
+
+    public void deleteUser(int userId);
+
+    public User getUser(int id);
+}
