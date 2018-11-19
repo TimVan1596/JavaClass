4790d9a3f3fe4799a4aae7dea3a4ae2c959ac32e
安天宝 <32336077@qq.com>
2018-11-19 16:29:47 +0800

    项目提交文档完成

---
+++ web/home/script/atb-shuffling.js
@@ -0,0 +1,10 @@
+layui.use(['table','carousel'], function(){
+    let carousel = layui.carousel;
+    //图片轮播
+    carousel.render({
+        elem: '#test'
+        ,width: 1000 //设置容器宽度
+        ,height: 200
+        ,anim: 'fade' //切换动画方式
+    });
+});
