a87e70e2304f093d8a00cbc192aaa4d4fd2a30b5
安天宝 <32336077@qq.com>
2018-11-21 11:28:20 +0800

    轮播更新

--- web/home/script/atb-shuffling.js
+++ web/home/script/atb-shuffling.js
@@ -4,7 +4,8 @@ layui.use(['table','carousel'], function(){
     carousel.render({
         elem: '#atb-shuffling'
         ,width: '100%' //设置容器宽度
-        ,height: 200
+        ,height: 250
+        // ,height: '22%'
         ,anim: 'fade' //切换动画方式
     });
 });
